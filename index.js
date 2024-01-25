746bb0e33047b66706b09d57a74905cc6d9b84c5
Billy <billybois68@gmail.com>
2024-01-25 21:35:40 +0700

    popup

--- index.js
+++ index.js
@@ -15,22 +15,33 @@ const listAnjing=[
 ]
 
 function openTab(evt, animalType) {
-    // Declare all variables
+
     let i, tabcontent, tablinks;
-  
-    // Get all elements with class="tabcontent" and hide them
+
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
     }
-  
-    // Get all elements with class="tablinks" and remove the class "active"
+
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
+      tablinks[i].className = tablinks[i].className.replace("active", "");
     }
-  
-    // Show the current tab, and add an "active" class to the button that opened the tab
+
     document.getElementById(animalType).style.display = "block";
     evt.currentTarget.className += " active";
   }
+
+// modal function
+function openPopup() {
+  modal = document.getElementById("modals");
+  modal.style.display = "block"
+}
+function closePopup() {
+    modal.style.display = 'none'
+}
+window.onclick = function(event) {
+    if (event.target==modal) {
+        modal.style.display = 'none'
+    }
+}
