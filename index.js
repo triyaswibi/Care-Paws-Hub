0a86544b5388ae8d83a56ab0c28125722e39c9b1
vgmon228 <vgmon228@gmail.com>
2024-01-25 18:23:42 +0800

    commit 1

---
+++ index.js
@@ -0,0 +1,36 @@
+//test
+const listAnjing=[
+    {
+        nama: "Pasien 1",
+        imgUrl: "./img/download (1).jpg",
+        deskripsi: "Mencret",
+        isSick: true
+    },
+    {
+        nama: "Pasien 2",
+        imgUrl: "./img/images.jpg",
+        deskripsi: "Mencret",
+        isSick: true
+    }
+]
+
+function openTab(evt, animalType) {
+    // Declare all variables
+    let i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(animalType).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
